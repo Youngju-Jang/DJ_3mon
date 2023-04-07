ceb28974b0b376b3e4f5849d0232626ad941d246
DESKTOP-OP39G2J\금정산1_PC19 <joj1043@nate.com>
2023-04-07 20:28:33 +0900

    0407(2) 자바 직렬화과제 수정

---
+++ jdcbConnect/src/com/bit/board/BoardMain.java
@@ -0,0 +1,63 @@
+package com.bit.board;
+
+import com.bit.util.DBUtil;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BoardMain {
+	BufferedReader in;
+	public BoardMain() {
+		in = new BufferedReader(new InputStreamReader(System.in));
+		menu();
+	}
+	private void menu() {
+		while (true) {
+			System.out.println("---------- 게시판 메뉴 ----------");
+			System.out.println("1. 글등록");
+			System.out.println("2. 글목록(전체)");
+			System.out.println("3. 글검색(제목)");
+			System.out.println("4. 글보기");
+			System.out.println("5. 글수정");
+			System.out.println("6. 글삭제");
+			System.out.println("-------------------------------------");
+			System.out.println("0. 프로그램 종료");
+			System.out.println("-------------------------------------");
+			System.out.print("메뉴 선택 : ");
+			try {
+				int num = Integer.parseInt(in.readLine());
+				switch (num) {
+				case 1:
+					registerArticle();
+					break;
+				case 2:
+					searchListAll();
+					break;	
+				default:
+					System.exit(0);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	private void registerArticle() throws Exception {
+		//BoardDto boardDto = new BoardDto();
+		System.out.println("registerArticle");
+	}
+	private void searchListAll() {
+		System.out.println("searchListAll");
+	}
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println(DBUtil.getInstance().getConnection());
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		BoardMain b = new BoardMain();
+		b.menu();
+	}
+
+}
