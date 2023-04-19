c148afc3ead1f44d1955625ba66a1387daf4829a
youngju-jang <joj1043@nate.com>
2023-04-19 20:52:15 +0900

    [DOCS] JS - 클로저, 호이스팅
    Related to #19

--- mysqlWork/필기/tunechp3.sql
+++ mysqlWork/필기/tunechp3.sql
@@ -1,32 +1,19 @@
 use tuning;
-show databases ;
-show tables;
-select count(1) from 사원;
 
-explain
-select * from 사원
-where 사원번호 between 100001 and 200000;
+show variables like 'profiling%';
+set profiling = 'ON';
 
-explain
-select 사원.사원번호, 급여.연봉
-from 사원,
-    (select 사원번호, max(연봉) as 연봉
-     from 급여
-     where 사원번호 between 10001 and 20000
-     group by 사원번호) as 급여
-where 사원.사원번호 = 급여.사원번호;
+select 사원번호 from 사원 where 사원번호 = 100000;
+show profiles;
+show profile for query 1;
 
-select * from 사원;
-
-explain
-select *
-from 사원
-where 사원번호 between 10001 and 100000
-    and 입사일자 = '1985-11-21';
-
-explain
-select 사원번호, 시작일자
-from 직급
-where 직급명 = 'Manager';
-
+Select count(1) from 직급;
 desc 직급;
+
+SELECT *
+FROM INFORMATION_SCHEMA.STATISTICS
+WHERE TABLE_SCHEMA = 'tuning';
+
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'tuning';
